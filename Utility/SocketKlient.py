5b04c95f9da727a3a3892466340c35dc8efff9b2
Neym4 <nikita08010456@mail.com>
2019-04-04 20:34:16 +0700

    Дополнение к клиенту

--- Utility/SocketKlient.py
+++ Utility/SocketKlient.py
@@ -1,39 +1,37 @@
 import socket
-#import vlc
-sock = socket.socket()
-sock.connect(('192.168.1.184', 9090))
-print('connect')
-data = sock.recv(1024)
-print(data)
-sock.send(b"Hello Daniel!")
-print("Hello Daniel!")
 
-
+# # import vlc
+# sock = socket.socket()
+# sock.connect(('192.168.1.184', 9090))
+# print('connect')
+# data = sock.recv(1024)
+# print(data)
+# sock.send(b"Hello Daniel!")
+# print("Hello Daniel!")
 
 
 # print(data)
 
-class KlientRPI ():
+class KlientRPI():
 
-    def sendTheMessage (self):
+    def sendTheMessage(self):
         print()
-
 
     def acceptMessege(self):
         print()
 
     def stopVideo(self):
-        #pygame.mixer.music.pause();
-        #global set
-        #set = 0  # set =0 ,means song is  not playing currently
-        print ('stop_Video')
+        # pygame.mixer.music.pause();
+        # global set
+        # set = 0  # set =0 ,means song is  not playing currently
+        print('stop_Video')
 
-    def playVideo (self):
-         #comm =  "/home/aayushshivam7/python\ projects/pygam_vlc.py "
-         #call(["python","pygam_vlc.py","abc.mp4"])
-        #global player
-        #player = vlc.MediaPlayer(r"C:\Users\admin\Desktop\sdfpidshjfkl\МЫ.mp4")
-        #player.play()
+    def playVideo(self):
+        # comm =  "/home/aayushshivam7/python\ projects/pygam_vlc.py "
+        # call(["python","pygam_vlc.py","abc.mp4"])
+        # global player
+        # player = vlc.MediaPlayer(r"C:\Users\admin\Desktop\sdfpidshjfkl\МЫ.mp4")
+        # player.play()
         print('Play video')
 
     # def StVid(self,event):
@@ -49,26 +47,49 @@ class KlientRPI ():
     def conn(self):
         print()
 
-    def Sturnofrscreen (self):
+    def Sturnofrscreen(self):
         print()
 
-    def Disconnect (self):
+    def Disconnect(self):
         print('disconnect')
 
-    def off_raspberry (self):
+    def off_raspberry(self):
         print("off_raspberry")
 
 t = KlientRPI()
-if data == b'run_video':
-    t.playVideo()
-if data == b'stop_video':
-    t.stopVideo()
-if data == b'off_raspberry':
-    t.off_raspberry()
 
-while True:
-    sock.connect(('192.168.1.184', 9090))
-    if data == b'Disconnect':
-        t.Disconnect()
 
-try:
+def takeMessage(message):
+    if message == b'run_video':
+        t.playVideo()
+    elif message == b'stop_video':
+        t.stopVideo()
+    elif message == b'off_raspberry':
+        t.off_raspberry()
+    else:
+        print("Error: " + message)
+
+
+if __name__ == '__main__':
+
+    r = True
+    sock = socket.socket()
+    while r:
+        try:
+            try:
+                print("Повтор подключения")
+                sock.connect(('192.168.1.184', 9090))
+                print('try connect')
+            except TimeoutError as e:
+                sock = socket.socket()
+                print("error " + str(e))
+            else:
+                while True:
+                    data = sock.recv(1024)
+                    takeMessage(data)
+        except BaseException as be:
+            sock = socket.socket()
+            print("error BE: " + str(be))
+        else:
+            pass
+
